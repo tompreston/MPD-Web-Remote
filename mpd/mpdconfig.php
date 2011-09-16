bafd144fceeab973a2c8010dfb0ec7a08618f355
Sven Ginka <sven.ginka@gmail.com>
2011-09-16 22:28:34 +0200

    mod: changes in config to make it suit to phpmpreloaded

--- mpd/mpdconfig.php
+++ mpd/mpdconfig.php
@@ -3,8 +3,16 @@
  *  mpdconfig.php
  *  Configuration for MPD
  */
+
+
+	/* some helpers for phpMpReloaded */
+	define('__PHPMPRELOADED_MPD_SETTINGS__', '../../../../config/mpd_config.php' );
+	define('__PHPMPRELOADED_CLIENT_SWITCHER__', '../../../../lib/ClientSwitcher.php');
+	if (file_exists( __PHPMPRELOADED_MPD_SETTINGS__ )){
+		include(__PHPMPRELOADED_MPD_SETTINGS__);
+	}
  
-  $host = 'localhost';
-  $mpdPort = 6600;
-  $mpdPassword = null;
+	$host = $mpd_host;
+	$mpdPort = $mpd_port;
+	$mpdPassword = null;
 ?>
